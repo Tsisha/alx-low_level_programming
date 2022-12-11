5c88c9e376e9104fb2490b1c23b3588c8f1456ea
Tsisha <temesgentsion2@gmail.com>
2022-12-11 00:16:34 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry Point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzel");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
