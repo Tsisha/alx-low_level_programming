5ee06876c082474d874e8465637a97374a49cd43
Tsisha <temesgentsion2@gmail.com>
2022-12-16 17:42:34 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,4 @@ void positive_or_negative(int i);
 int largest_age(int a, int b, int c);
 void print_remaining_birthdays(int month, int day, int year);
 int convert_day(int month, int day);
-
 #endif
