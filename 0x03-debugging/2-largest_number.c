421f34082532bce03d2178f52b5eeda0d50e0f20
Tsisha <temesgentsion2@gmail.com>
2022-12-17 15:08:34 -0800

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - prints the largest of 3 intege
+ * Return: 0
+ */
+int main(void)
+{
+int a, b, c;
+int largest;
+a = 972;
+b = -98;
+c = 0;
+largest = a, b, c;
+printf("%d", largest);
+return (0);
+}
