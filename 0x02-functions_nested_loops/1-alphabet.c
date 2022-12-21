ab9fbca1b3c2a0b307059665b827f3f3ed917e5a
Tsisha <temesgentsion2@gmail.com>
2022-12-21 03:31:24 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void);
+for (int x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = tolower(x);
+_putchar (lower_x);
+_putchar ('\n');
+}
