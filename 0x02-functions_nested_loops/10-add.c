bd9e256cc49c7a4e28472c4e365760461076b2a1
Tsisha <temesgentsion2@gmail.com>
2022-12-25 08:08:34 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds_two_integers
+ * @inx: the first integer to be computed
+ * @iny: the second integer to be computed
+ * Return: integer value
+ */
+int add(int inx, int iny)
+{
+return (inx + iny);
+}
