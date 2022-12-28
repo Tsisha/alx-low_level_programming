44c85734302262f20fb2558267704a3ddb24bc26
Tsisha <temesgentsion2@gmail.com>
2022-12-27 18:45:21 -0800

    1 Task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,12 @@ int main(void)
 int i;
 printf("infinite loop incoming :(\n");
 i = 0;
-/**
+/*
  *  while (i < 10)
  *  {
- *  putchat(i);
+ *  putchar(i);
  *  }
  */
-printf("infinite loop avoided! \\o/n");
+printf("infinite loop avoided! \\o/\n");
 return (0);
 }
