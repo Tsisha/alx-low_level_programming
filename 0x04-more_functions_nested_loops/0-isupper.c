a26fe238532ed82cd3e6919d6951cc43fd57acb4
Tsisha <temesgentsion2@gmail.com>
2022-12-29 20:30:43 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,11 @@
 /**
  * _isupper - A function that checks for upper case character.
  * @c: An input character
- *
- * Return: 1 if c is uppercase or 0 otherwise
+ * Return: Always 0
  */
 int _isupper(int c)
 {
-if (c >= '65' && c <= '90')
+if (c >= 65 && c <= 90)
 {
 return (1);
 }
