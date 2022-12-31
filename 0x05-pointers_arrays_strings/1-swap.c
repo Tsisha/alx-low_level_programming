467ea6e103a777d4c0e53d5386962b1720dbac3a
Tsisha <temesgentsion2@gmail.com>
2022-12-31 01:14:59 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-swap.c -o 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+int x;
+x = 0;
+x = *a;
+*a = *b;
+*b = x;
+}
