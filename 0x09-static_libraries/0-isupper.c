3c09b0e9a355c71046512adbe9b71b02fb8b02dc
Tsisha <temesgentsion2@gmail.com>
2023-01-06 14:32:03 -0800

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"           
+/**           
+ * _isupper - A function that checks for upper case character.
+ * @c: An input character
+ * Return: Always 0                
+ */  
+int _isupper(int c)               
+{
+if (c >= 65 && c <= 90)               
+{        
+return (1);               
+}        
+return (0);       
+}
